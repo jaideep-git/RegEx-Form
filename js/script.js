9530ea1f508c276a7e4080a795f8fb01f428f8db
Jaideep <78426970+jaideep-git@users.noreply.github.com>
2022-02-08 17:24:52 -0500

    fields Validation complete

--- js/script.js
+++ js/script.js
@@ -5,23 +5,12 @@ $(document).ready(function(){
     });
 })
 
-document.querySelector("#firstNameError").innerText= "First Name*";
-document.querySelector("#lastNameError").innerText= "Last Name";
-
 //*** Form Validation using RegEx ***//
 
 let fieldsChecked;
 
 function hasCharCheck(dataToCheck){
     let pattern = /^[a-zA-Z]+$/;
-    if(pattern.test(dataToCheck.field.value)){
-        return true;
-    }
-    return false;
-}
-
-function hasDateCheck(dataToCheck){
-    let pattern = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/;
     if(pattern.test(dataToCheck.field.value)){
         return true;
     }
@@ -36,10 +25,43 @@ function hasEmailValid(dataToCheck ){
     return false;
 }
 
+function phoneNumberCheck(dataToCheck){
+    let pattern = /^[#.0-9a-zA-Z\s,-]+$/;
+    if(pattern.test(dataToCheck.field.value)){
+        return true;
+    }
+    return false;
+}
+
+function addressCheck(dataToCheck){
+    let pattern =  /^[A-Za-z0-9\s#()+-`\\\]\|'/.]{3,}$/; 
+    if(pattern.test(dataToCheck.field.value)){
+        return true;
+    }
+    return false;
+}
+
+function cityValidation(dataToCheck){
+    let pattern = /^(?:[A-Za-z]{2,}(?:(\.\s|'s\s|\s?-\s?|\s)?(?=[A-Za-z]+))){1,2}(?:[A-Za-z]+)?$/;
+    if(pattern.test(dataToCheck.field.value)){
+        return true;
+    }
+    return false;
+}
+
+function postalCodeValidation(dataToCheck){
+    let pattern = /[A-Za-z]\d[A-Za-z] ?\d[A-Za-z]\d/;
+    if(pattern.test(dataToCheck.field.value)){
+        return true;
+    }
+    return false;
+}
+
 // Function for resetting the errors after form submisson 
 function resetErrors(){
     fieldsChecked.forEach(inputField =>{
         inputField.error.innerText = "";
+        inputField.field.style.border = "1px solid grey"
     })
 }
 
@@ -51,11 +73,17 @@ function formClickHandler(e){
 
     // * automated error message
     fieldsChecked.forEach(inputField => {
-        if(inputField.checker(inputField)==false){
+        if(inputField.field.value == ""){
+            inputField.error.innerText= "This field is required";
+            inputField.field.style.border= "none"
+            inputField.field.style.borderBottom = "1px solid red"
+        }
+        else if (inputField.checker(inputField)==false){
             inputField.error.innerText= inputField.msg;
             errorsFound +=1;
         }
     })
+    window.scrollTo(0, 0);
 }
 
 // * Grabbing input and error fields
@@ -68,13 +96,25 @@ function initForm() {
     let dateBirthError = document.querySelector("#dobError");
     let email = document.querySelector("#email");
     let emailError = document.querySelector("#emailAddressError");
+    let phoneNumber = document.querySelector("#mobileNumber");
+    let phoneNumbeError = document.querySelector("#mobileNumberError");
+    let address = document.querySelector('#address');
+    let addressError = document.querySelector('#addressError');
+    let city = document.querySelector('#city');
+    let cityError = document.querySelector('#cityError');
+    let postalCode = document.querySelector('#postalCode');
+    let postalCodeError = document.querySelector('#postalCodeError');
 
     // * Declaring data objects for checking the information entered by the user
     fieldsChecked = [
         {field:firstName , checker: hasCharCheck , error: firstNameError, msg:"Please enter a Valid first name"},
         {field:lastName , checker: hasCharCheck , error: lastNameError, msg:"Please enter a Valid last name"},
-        {field:dateBirth , checker: hasDateCheck , error: dateBirthError, msg:"Please enter a Valid Date of birth"},
-        {field:email , checker:hasEmailValid , error: emailError, msg:"Please enter a Valid email address"}
+        {field:dateBirth , error: dateBirthError, msg:"Please enter a Valid Date of birth"},
+        {field:email , checker:hasEmailValid , error: emailError, msg:"Please enter a Valid email address"},
+        {field:phoneNumber , checker:phoneNumberCheck, error: phoneNumbeError, msg:"Phone number must be 10 digits"},
+        {field:address , error: addressError,  checker: addressCheck,  msg:"Please enter valid address"},
+        {field:city , checker:cityValidation , error: cityError, msg:"Please enter a valid city name"},
+        {field:postalCode , checker:postalCodeValidation , error: postalCodeError, msg:"Please enter a valid city name"},
     ]
     let submitForm = document.querySelector("#submit");
     submitForm.addEventListener("click", formClickHandler);
